47f2931e80ed455c2cfd1e67f2faec2ceed587a8
sdercolin <sder.colin@gmail.com>
2023-10-30 00:33:28 +0900

    Adjust theme docs

--- shared/src/commonMain/kotlin/ui/style/Theme.kt
+++ shared/src/commonMain/kotlin/ui/style/Theme.kt
@@ -6,24 +6,43 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Light color palette for the app.
+ * - Primary: Orange 900
+ * - Primary variant: Deep Orange 900
+ * - Secondary: Yellow 600
+ * - Secondary variant: Amber 800
+ * - Surface: Grey 200
+ * - Background: Grey 100
+ * - Error: Red 700
+ */
 val appLightColors = lightColors(
-    primary = Color(0xffe65100), // Orange 900
-    primaryVariant = Color(0xffbf360c), // Deep Orange 900
-    secondary = Color(0xfffdd835), // Yellow 600
-    secondaryVariant = Color(0xffff8f00), // Amber 800
-    surface = Color(0xffEEEEEE), // Grey 200
-    background = Color(0xfff5f5f5), // Grey 100
-    error = Color(0xffd32f2f), // Red 700
+    primary = Color(0xffe65100),
+    primaryVariant = Color(0xffbf360c),
+    secondary = Color(0xfffdd835),
+    secondaryVariant = Color(0xffff8f00),
+    surface = Color(0xffEEEEEE),
+    background = Color(0xfff5f5f5),
+    error = Color(0xffd32f2f),
 )
 
+/**
+ * Dark color palette for the app.
+ * - Primary: Yellow 800
+ * - Primary variant: Orange 900
+ * - Secondary: Yellow 600
+ * - Secondary variant: Yellow 600
+ * - Surface: between Grey 800 and 900
+ * - Background: larger than Grey 900
+ */
 val appDarkColors = darkColors(
-    primary = Color(0xfff9a825), // Yellow 800
-    primaryVariant = Color(0xffe65100), // Orange 900
-    secondary = Color(0xfffdd835), // Yellow 600
-    secondaryVariant = Color(0xfffdd835), // Yellow 600
-    surface = Color(0xff252525), // Grey 800~900
-    background = Color(0xff1e1e1e), // Grey 900++
-    error = Color(0xfff44336), // Red 500
+    primary = Color(0xfff9a825),
+    primaryVariant = Color(0xffe65100),
+    secondary = Color(0xfffdd835),
+    secondaryVariant = Color(0xfffdd835),
+    surface = Color(0xff252525),
+    background = Color(0xff1e1e1e),
+    error = Color(0xfff44336),
 )
 
 @Composable
