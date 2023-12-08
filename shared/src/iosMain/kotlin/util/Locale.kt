f52cc2b74109458f48b749cd08e966d500a03e21
sdercolin <sder.colin@gmail.com>
2023-12-08 18:09:42 +0900

    Fix Locale on iOS

--- shared/src/iosMain/kotlin/util/Locale.kt
+++ shared/src/iosMain/kotlin/util/Locale.kt
@@ -4,8 +4,14 @@ import platform.Foundation.NSLocale
 import platform.Foundation.countryCode
 import platform.Foundation.currentLocale
 import platform.Foundation.languageCode
+import platform.Foundation.preferredLanguages
 
 actual val Locale: String by lazy {
+    Log.d("preferredLanguages: ${NSLocale.preferredLanguages.joinToString(", ")}")
+    val preferredLanguage = NSLocale.preferredLanguages.firstOrNull() as? String
+    if (preferredLanguage != null) {
+        return@lazy preferredLanguage
+    }
     val locale = NSLocale.currentLocale
     "${locale.languageCode}-${locale.countryCode}"
 }
