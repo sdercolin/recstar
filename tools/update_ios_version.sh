84d38351c75a454e735bf796e486e14be90921b0
sdercolin <sder.colin@gmail.com>
2024-01-11 05:37:43 +0900

    Remove unusable tool

--- tools/update_ios_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PLIST=../iosApp/iosApp/Info.plist
-VERSION_NAME=$1
-VERSION_CODE=$2
-
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $VERSION_NAME" "$PLIST"
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $VERSION_CODE" "$PLIST"
